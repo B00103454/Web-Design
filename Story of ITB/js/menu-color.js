98fecf0fc60be71db4c3960009f583f6cd7e631d
B00103454 <45840290+B00103454@users.noreply.github.com>
2018-12-13 06:25:31 +0000

    Add files via upload

---
+++ Story of ITB/js/menu-color.js
@@ -0,0 +1,29 @@
+
+   $(window).scroll(function(){
+	   "use strict";
+    if ($(this).scrollTop() > 350) {
+       $('.icon-bar').addClass('opaque');
+       $('.navbar-brand img').attr('src', 'img/logo-light.png');
+    } else {
+       $('.icon-bar').removeClass('opaque');
+       $('.navbar-brand img').attr('src', 'img/logo.png');
+    }
+});
+
+
+
+$(function(){
+		   "use strict";
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100) {
+            $('#topbar, .cart-label').fadeOut('slow');
+            $('.logo img')
+                .attr('src','img/logo.png');
+        }
+        if($(this).scrollTop() < 100) {
+            $('#logo, #topbar, .cart-label').fadeIn('fast');
+            $('.logo img')
+                .attr('src','img/logo-light.png');
+        }
+    });
+});
